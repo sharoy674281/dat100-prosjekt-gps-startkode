8e1bf43e253c026701e3b36c078c21d0d9721e50
Lars Michael Kristensen <lmkr@users.noreply.github.com>
2024-09-25 11:33:34 +0200

    oppgave 2 update

--- src/no/hvl/dat100ptc/oppgave2/Main.java
+++ src/no/hvl/dat100ptc/oppgave2/Main.java
@@ -7,17 +7,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 
-		GPSPoint gpspoint1 = new GPSPoint(1, 2.0, 3.0, 5.0);
+		// TODO
 		
-		GPSPoint gpspoint2 = new GPSPoint(2, 3.0, 4.0, 7.0);
-
-		GPSData gpsdata = new GPSData(2);
-		
-		gpsdata.insertGPS(gpspoint1);
-		
-		gpsdata.insertGPS(gpspoint2);
-		
-		gpsdata.print();
-
 	}
 }
